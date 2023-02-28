f9a076bfccb986d44d1926fef4531e69e665f019
admin <admin@Bindus-MacBook-Air.local>
2023-02-28 18:41:43 +0530

    setup change

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='cg_etcd',
-    packages=['cg_etcd'],
+    packages=find_packages(),
     description='etcd Library for Credgenics',
     version='',
     url='',
     author='Rohit Choudhary',
     author_email='rohit.choudhary@credgenics.com',
     install_requires = [
-        "etcd3==0.12.0","protobuf==3.19.6",
+        "etcd3==0.12.0","protobuf==3.19.6"
     ]
 )
